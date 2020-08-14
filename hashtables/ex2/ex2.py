fe3a7bd90d1c0de7038f24e5a673202f8dc7493e
adella <scratchglory@gmail.com>
2020-08-14 19:43:54 -0400

    finished ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,35 @@ class Ticket:
         self.destination = destination
 
 
+# source == start
+# destination == next airport
+# first flight has source == NONe
+# final flight has source == None
+# start with None, match destination with source, add to route
+# match prev.destination to next.source, add destination to route
+# repeat / use links
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # empty cache to keep track
+    cache = {}
+    route = [None] * length
+
+    # loop through tickets list to assign source to destination
+    for tx in tickets:
+        cache[tx.source] = tx.destination
+        # print("source", tx.source)
+        # print("destination", cache[tx.source])
+
+    # to compare and find NONE, will be overwritten if not NONE
+    dest = cache["NONE"]
+    print(cache)
+    # loop through each index through route
+    for i in range(length):
+        print("dest", dest)
+        route[i] = dest
+        dest = cache[dest]
 
     return route
